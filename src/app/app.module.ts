4ea8df1cac687a208a81ee7586cac4b67d25a3c2
Lorene Valency <lorene.herzog@gmail.com>
2019-01-11 01:09:31 +0200

    add movie item

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { MoviesComponent } from './movies/movies.component';
 import { HeaderComponent } from './header/header.component';
-import { MovieDetailComponent } from './movies/movie-detail/movie-detail.component';
 import { MovieListComponent } from './movies/movie-list/movie-list.component';
+import { MovieItemComponent } from './movies/movie-list/movie-item/movie-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MoviesComponent,
     HeaderComponent,
-    MovieDetailComponent,
-    MovieListComponent
+    MovieListComponent,
+    MovieItemComponent
   ],
   imports: [
     BrowserModule
