3f7cfa8d501fcc088a4114fb0fe680341d1f0439
Lorene Valency <lorene.herzog@gmail.com>
2019-01-12 10:00:45 +0200

    fixed components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
 import { AppComponent } from './app.component';
 import { MoviesComponent } from './movies/movies.component';
 import { HeaderComponent } from './header/header.component';
-import { MovieListComponent } from './movies/movie-list/movie-list.component';
-import { MovieItemComponent } from './movies/movie-list/movie-item/movie-item.component';
+import { MovieItemComponent } from './movies/movie-item/movie-item.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     MoviesComponent,
     HeaderComponent,
-    MovieListComponent,
     MovieItemComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/movies/movie-item/movie-item.component.ts
+++ src/app/movies/movie-item/movie-item.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { Movie } from '../../movie.model';
+import { Movie } from '../movie.model';
 
 @Component({
   selector: 'app-movie-item',
