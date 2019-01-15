e4b89a41168a7f35fded38c130e052c166f1ddfc
Lorene Valency <lorene.herzog@gmail.com>
2019-01-15 17:43:14 +0200

    design

--- src/app/movies/movie-title.pipe.ts
+++ src/app/movies/movie-title.pipe.ts
@@ -3,7 +3,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 @Pipe({
   name: 'removeSpecialChar'
 })
-
+//Custom Pipe used to remove all special characters from the titles inputed by the user
 export class MovieTitlePipe implements PipeTransform {
   transform(value: string): string {
     let newVal = value.replace(/[^\w\s]/gi, '')
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -6,6 +6,9 @@ import { forkJoin } from 'rxjs';
 import { MovieService } from '../movies/movie.service';
 import { Movie } from '../movies/movie.model';
 
+//This is a JS function that transforms keys of an object to camelCase.
+//This function is used because the API's keys have a capital letter as a first char
+//which is not suitable with our model attributes
 declare function toCamel(o);
 
 @Injectable()
@@ -14,46 +17,42 @@ export class DataStorageService {
   error: any;
   movies: Movie[] = [];
 
-
-  getMovies(query?: string) {
-
+ //requests for movies from the API with s="query"
+ //this request returns a list of movies with the "query" but there are only
+ //the image, the title and the ID available
+  getMovies(query: string) {
     return this.http.get<Movie[]>(this.getMovieUrlS(query));
-
   }
 
+  //Fetch the movies with the previous request in order to get all the titles
+  //Stores the titles in an Array
+  //sendind requests on each title from the API with t="query"
+  //these requests return only one movie each but with every information that we want
   fetchMovies(query: string) {
     let titles = [];
     let titleRequests = [];
     this.getMovies(query).subscribe((movies: Movie[]) => {
-
-        this.movieService.setMovies(toCamel(movies)["search"]);
-
-        for(let i = 0; i < this.movies.length; i++) {
-          titles.push(this.movieService.seeMovies()[i]['title']);
-        }
-        for(let i = 0; i < titles.length; i++) {
-          titleRequests.push(this.http.get(this.getMovieUrlT(titles[i])));
-        }
-
-        forkJoin(titleRequests).subscribe(
-          (movies: Movie[]) => {
-            this.movieService.setMovies(toCamel(movies))
+      this.movieService.setMovies(toCamel(movies)["search"]);
+      for(let i = 0; i < this.movies.length; i++) {
+        titles.push(this.movieService.seeMovies()[i]['title']);
+      }
+      for(let i = 0; i < titles.length; i++) {
+        titleRequests.push(this.http.get(this.getMovieUrlT(titles[i])));
+      }
+      forkJoin(titleRequests).subscribe(
+        (movies: Movie[]) => {
+          this.movieService.setMovies(toCamel(movies))
         })
-
     }),
-
     this.movieService.moviesChanged.subscribe(
       (movies: Movie[]) => {
         this.movies = movies;
       }
     );
-
     if (this.movies.length = 0) {
       this.error = '0 retults found';
     }
-
   }
-
 
   private getMovieUrlT(query: string): string {
     return `https://www.omdbapi.com/?t=${query}&apikey=6d4e8e6`;
